345df25e8722ecb0651e8742941932d7c2db7a02
Freja Kammersgaard <308840@viauc.dk>
2021-05-23 01:37:22 +0200

    freja

---
+++ ViaClub/src/Model/MyDate.java
@@ -0,0 +1,60 @@
+package Model;
+import java.util.GregorianCalendar;
+
+public class MyDate {
+    private int day;
+    private int month;
+    private int year;
+
+    public MyDate(int day, int month, int year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    public MyDate() {
+        MyDate date = today();
+        this.day = date.day;
+        this.month = date.month;
+        this.year = date.year;
+    }
+
+    public static MyDate today() {
+        GregorianCalendar currentDate = new GregorianCalendar();
+        int currentDay = currentDate.get(GregorianCalendar.DATE);
+        int currentMonth = currentDate.get(GregorianCalendar.MONTH) + 1;
+        int currentYear = currentDate.get(GregorianCalendar.YEAR);
+
+        return new MyDate(currentDay, currentMonth, currentYear);
+    }
+
+    public void setDay(int day) {
+        if (day == 0) {
+            throw new IllegalDataException(day);
+        } else
+            this.day = day;
+    }
+
+    public void setMonth(int month) {
+        if (day == 0) {
+            throw new IllegalDataException(month);
+        } else
+            this.month = month;
+    }
+
+    public void setYear(int year) {
+        if (day == 0) {
+            throw new IllegalDataException(year);
+        } else
+            this.year = year;
+    }
+
+    public String toString() {
+        return this.day + "/" + this.month + "/" + this.year;
+    }
+
+    public MyDate copy() {
+        return new MyDate(this.day, this.month, this.year);
+    }
+}
+
