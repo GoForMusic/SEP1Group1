6f9b164191a4c0a963549c53014a7957c4ddd8bb
Adrian Militaru <goformusicro@gmail.com>
2021-05-26 10:21:42 +0200

    finish MatchGUI 2, add player assign/remove from a match add some comments for this matter.

--- ViaClub/src/Model/VIAClub.java
+++ ViaClub/src/Model/VIAClub.java
@@ -23,50 +23,14 @@ public class VIAClub
     matches.add(match);
   }
 
-  public void removePlayer(Player player)
-  {
-    players.remove(player);
-  }
-
-  public void removeMatch(Match match)
-  {
-    matches.remove(match);
-  }
-
-  public Player getPlayers(int index)
-  {
-    return players.get(index);
-  }
-
   public ArrayList<Player> getPlayers()
   {
     return players;
-  }
-  public Match getMatch(int index)
-  {
-    return matches.get(index);
   }
 
   public ArrayList<Match> getMatches()
   {
     return matches;
-  }
-  public void editPlayers(int index, Player player)
-  {
-    players.get(index).setFirstName(player.getFirstName());
-    players.get(index).setLastName(player.getLastName());
-    players.get(index).setPlayerNumber(player.getPlayerNumber());
-    players.get(index).setPreferredPosition(player.getPreferredPosition());
-    players.get(index).setStatus(player.getStatus());
-  }
-  public void editMatches(int index, Match match)
-  {
-  matches.get(index).setDate(match.getDate());
-  matches.get(index).setLocation(match.getLocation());
-  matches.get(index).setType(match.getType());
-  matches.get(index).setTeam1(match.getTeam1());
-  matches.get(index).setTeam2(match.getTeam2());
-  matches.get(index).setScore(match.getScore());
   }
 
  public String toString()
